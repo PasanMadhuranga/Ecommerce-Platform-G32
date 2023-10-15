e07fac27f3a87e25e60e1557b690782582fc75d6
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2023-10-15 07:26:47 +0530

    create 'addUser' procedure
    Create a procedure to add a new user

--- database/SQL_procedures.sql
+++ database/SQL_procedures.sql
@@ -74,3 +74,41 @@ BEGIN
 END //
 
 DELIMITER ;
+
+
+-- insert an user into the customer table
+-- 1. If the user is registered, send an error message
+-- 2. If the user is a new guest, insert the user into the customer table
+-- 3. If the user has bought something as a guest, update the user's information
+DROP PROCEDURE IF EXISTS addUser;
+
+DELIMITER //
+
+CREATE PROCEDURE addUser( 
+    p_password VARCHAR(50),
+    p_first_name VARCHAR(50), 
+    p_last_name VARCHAR(50),
+    p_email VARCHAR(255), 
+    p_phone_num CHAR(10),
+    p_address_l1 VARCHAR(50), 
+    p_address_l2 VARCHAR(50), 
+    p_city VARCHAR(50), 
+    p_province VARCHAR(50), 
+    p_zip CHAR(5))
+BEGIN
+    DECLARE user_status INT;
+    SELECT isTheUserRegistered(p_email) INTO user_status;
+
+    IF (user_status = 1) THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'User already exists'; -- error
+    ELSEIF (user_status = -1) THEN
+        INSERT INTO customer (Password, First_name, Last_name, Email, Phone_number, Address_line1, Address_line2, City, Province, Zipcode, Is_registered)
+        VALUES (p_password, p_first_name, p_last_name, p_email, p_phone_num, p_address_l1, p_address_l2, p_city, p_province, p_zip, 1);
+    ELSE
+        UPDATE customer
+        SET Password = p_password, First_name = p_first_name, Last_name = p_last_name, Phone_number = p_phone_num, Address_line1 = p_address_l1, Address_line2 = p_address_l2, City = p_city, Province = p_province, Zipcode = p_zip, Is_registered = 1
+        WHERE Email = p_email;
+    END IF;
+END //
+
+DELIMITER ;
