bdf6ab17a87a9bd145b8ceb1056b775782a23c5c
Iruda Chirath <iruda.21@cse.mrt.ac.lk>
2023-10-17 00:33:13 +0530

    Update insertions

--- database/insertions.sql
+++ database/insertions.sql
@@ -1,5 +1,5 @@
 -- add product
-
+-- product_id = 1
 call `group32_v1.0`.add_product(
 	'iPhone 14', 
     'Electronics, Smartphones', 
@@ -7,7 +7,7 @@ call `group32_v1.0`.add_product(
     0.22, 
     'IP14', 
     'https://drive.google.com/file/d/1JvgLUWiq-0dHII3lRW44S6Q4Aug0qnuE/view?usp=sharing');
-
+-- product_id = 2
 call `group32_v1.0`.add_product(
 	'Samsung Galaxy S22', 
     'Electronics, Smartphones', 
@@ -15,7 +15,7 @@ call `group32_v1.0`.add_product(
     0.24, 
     'SGS22', 
     'https://drive.google.com/file/d/1nRxreqjfaBKqC4yW2GhJcP3QfR7DBJNg/view?usp=sharing');
-
+-- product_id = 3
 call `group32_v1.0`.add_product(
 	'Dell XPS 15', 
     'Electronics, Laptops', 
@@ -23,7 +23,7 @@ call `group32_v1.0`.add_product(
     1.81, 
     'DXPS15', 
     'https://drive.google.com/file/d/1XQ597X9Ur0K-UClDLR78i5jXuT8ngzQf/view?usp=sharing');
-    
+-- product_id = 4    
 call `group32_v1.0`.add_product(
 	'Sony WH-1000XM5', 
     'Electronics, Audio & Headphones', 
@@ -31,7 +31,7 @@ call `group32_v1.0`.add_product(
     0.25, 
     'SWH5', 
     'https://drive.google.com/file/d/1NhIAxxqvgixIfHJfC2hIaK03_0F97BzR/view?usp=sharing');
-    
+-- product_id = 5   
 call `group32_v1.0`.add_product(
 	'Batman Action Figure', 
     'Toys, Action Figures', 
@@ -39,7 +39,143 @@ call `group32_v1.0`.add_product(
     0.15, 
     'BAF', 
     'https://drive.google.com/file/d/1waw9Mt7n3jCKSbPARFX3u0eNIhy_Vi-p/view?usp=sharing');
-
+-- product_id = 6
+call `group32_v1.0`.add_product(
+	'Bose QC45', 
+    'Electronics, Audio & Headphones', 
+    'Bose QC45: The Bose QC45 headphones redefine your audio experience. With world-class noise cancellation, you can enjoy your music with newfound clarity. The balanced sound profile ensures every note is crisp and clear. Lightweight and comfortable, these headphones are perfect for long journeys or peaceful moments of reflection.', 
+    0.23, 
+    'BQC45', 
+    'https://drive.google.com/file/d/1I8_Y-GB2BDFS5I7l5Z4zCl9Rj9zf0cHE/view?usp=sharing');
+-- product_id = 7
+call `group32_v1.0`.add_product(
+	'Google Pixel 8 pro', 
+    'Electronics, Smartphones', 
+    'Google Pixel 8 Pro: 24+ hour battery charges faster than ever. Powerful performance with 12 GB RAM. Pixel helps keep you and your info safe. 7 years of security updates and new Feature Drops. The updated Pixel 8 Pro camera system makes selfies sharper, close-ups closer, and photos and videos more stunning than ever.', 
+    0.18, 
+    'GP8P', 
+    'https://drive.google.com/file/d/1q7QPcBpwEnlXYzzN9CJmmQy963XXg_u4/view?usp=sharing');
+-- product_id = 8
+call `group32_v1.0`.add_product(
+	'OnePlus 11', 
+    'Electronics, Smartphones', 
+    'OnePlus 11: 50 MP (wide) + 32 MP (telephoto) + 48 MP (ultrawide) while on the front, there is a 16 MP (wide) sensor. The display size is 6.7 inches LTPO2 AMOLED. The resolution of the screen is 1440 x 3216 pixels.', 
+    0.20, 
+    '1P11', 
+    'https://drive.google.com/file/d/1cUS-l9J8tb4KFdrDtyS1LEr8QTT3RpvJ/view?usp=sharing');
+-- product_id = 9
+call `group32_v1.0`.add_product(
+	'Asus ZenBook Pro', 
+    'Electronics, Laptops', 
+    'Asus ZenBook Pro: The Asus ZenBook Pro is a masterpiece of modern laptop design. With its stunning 4K OLED touchscreen display, it offers vivid visuals and precise touch control. Inside, the laptop is powered by high-performance processors, making it a powerhouse for creative professionals. The sleek aluminum body and compact form factor make it a stylish and portable companion. Whether you\'re a designer, video editor, or simply someone who appreciates top-tier technology, the ZenBook Pro is a testament to Asus\'s commitment to innovation.', 
+    1.75, 
+    'AZBP', 
+    'https://drive.google.com/file/d/1Dw2q9ZkL0k0o8aitG5eZ0WMlDixw2sAW/view?usp=sharing');
+-- product_id = 10
+call `group32_v1.0`.add_product(
+	'MacBook Air M2', 
+    'Electronics, Laptops', 
+    'MacBook Air M2: The MacBook Air M2 is Apple\'s answer to the need for a lightweight, ultra-portable laptop without compromising on performance. Powered by the M2 chip, it provides snappy responsiveness and excellent energy efficiency. The Retina display offers vibrant visuals, and the compact form factor makes it ideal for on-the-go professionals and students. With a sleek aluminum chassis, all-day battery life, and the signature MacBook keyboard and trackpad, the MacBook Air M2 is a modern classic for those who value mobility and style.', 
+    1.25, 
+    'MBA2', 
+    'https://drive.google.com/file/d/1Oi8NKX7JkyKD5eNt0yjhwyvlrgzNVMjw/view?usp=sharing');
+-- product_id = 11
+call `group32_v1.0`.add_product(
+	'Beats Studio Buds Plus', 
+    'Electronics, Audio & Headphones', 
+    'Beats Studio Buds Plus: The Beats Studio Buds Plus are the perfect audio companion for music lovers and active individuals. With their dynamic sound quality, these true wireless earbuds deliver immersive audio experiences. They\'re designed for comfort and durability, ensuring they stay in place during workouts and daily activities. The active noise cancellation feature lets you enjoy your music in peace, and the long battery life keeps the tunes flowing all day long. Connect seamlessly to your device and elevate your audio experience with these stylish earbuds.', 
+    0.05, 
+    'BSBP', 
+    'https://drive.google.com/file/d/1BK9sXKBWyUg3c3RgKjsxueHi4G2B7jWV/view?usp=sharing');
+-- product_id = 12
+call `group32_v1.0`.add_product(
+	'Jabra Elite 85t', 
+    'Electronics, Audio & Headphones', 
+    'Jabra Elite 85t: The Jabra Elite 85t true wireless earbuds redefine what\'s possible in terms of audio quality and noise cancellation. These earbuds offer crystal-clear sound and advanced noise-canceling technology, allowing you to immerse yourself in your music or make clear calls, even in noisy environments. With a comfortable fit and long battery life, they are perfect for extended listening sessions or all-day productivity. Stay connected and enjoy an audio experience like never before with the Jabra Elite 85t.', 
+    0.06, 
+    'JE85', 
+    'https://drive.google.com/file/d/1PRND2SeP1DTBlOIu9-29jwB3zCtym-p4/view?usp=sharing');
+-- product_id = 13
+call `group32_v1.0`.add_product(
+	'Ironman Action Figure', 
+    'Toys, Action Figures', 
+    'Ironman Action Figure: The Ironman action figure is a tribute to the genius, billionaire, playboy, philanthropist, Tony Stark. With exquisite detailing and a range of articulation points, it brings Ironman\'s armored suit to life. Whether you\'re a Marvel fan or simply appreciate fine craftsmanship, this action figure allows you to display one of Earth\'s mightiest heroes with pride.', 
+    0.15, 
+    'IAF', 
+    'https://drive.google.com/file/d/1y9vJDVoOncF89tCZNAgNkFWX2HzVTGH_/view?usp=sharing');
+-- product_id = 14
+call `group32_v1.0`.add_product(
+	'Lego Star Wars Kit', 
+    'Toys, Board Games', 
+    'Lego Star Wars Kit: The Lego Star Wars Kit is an adventure in creativity and imagination. With a vast collection of Lego bricks and minifigures inspired by the iconic Star Wars universe, it offers endless possibilities for building your favorite spacecraft, characters, and scenes. Whether you\'re a Star Wars enthusiast or a Lego aficionado, this kit provides hours of entertainment and storytelling. Join the Rebel Alliance or embrace the dark side with this epic Lego set.', 
+    0.9, 
+    'LSWK', 
+    'https://drive.google.com/file/d/1JS_aazbIBVPtReUJMWCBiIcTStSwtAeM/view?usp=sharing');
+-- product_id = 15
+call `group32_v1.0`.add_product(
+	'Canon EOS 7D', 
+    'Electronics, Cameras & Photography', 
+    'Canon EOS 7D: The Canon EOS 7D is a professional DSLR camera designed for photographers who demand precision and speed. With its fast autofocus system and high-resolution sensor, it captures stunning images with exceptional clarity. Whether you\'re capturing fast-action sports or intricate landscapes, the EOS 7D excels in every scenario. Its robust build and advanced features make it a versatile tool for photographers who push the boundaries of creativity.', 
+    0.8, 
+    'CE7D', 
+    'https://drive.google.com/file/d/1Sl4CSaoJc-rTPjR5-ayy7YSsrAzg1LQ1/view?usp=sharing');
+-- product_id = 16
+call `group32_v1.0`.add_product(
+	'Apple Watch Series 8', 
+    'Electronics, Wearable Technology', 
+    'Apple Watch Series 8: The Apple Watch Series 8 represents the pinnacle of smartwatch technology. With its elegant design and advanced health and fitness tracking features, it\'s more than just a watch—it\'s your personal wellness companion. The Series 8 offers precise tracking of your heart rate, sleep, activity, and more, helping you stay on top of your health and fitness goals. With an always-on display and seamless integration with your iPhone, it\'s the ultimate accessory for a connected and active lifestyle. Experience the future of wearables with the Apple Watch Series 8.', 
+    0.07, 
+    'AWS8', 
+    'https://drive.google.com/file/d/1e7eMwnu59yojB9n4uO2cWI8gk3nTqImv/view?usp=sharing');
+-- product_id = 17
+call `group32_v1.0`.add_product(
+	'Logitech G Pro Mouse', 
+    'Electronics, Computer Accessories', 
+    'Logitech G Pro Mouse: The Logitech G Pro Mouse is a gaming mouse that sets the standard for precision and performance. Designed with input from esports professionals, it offers lightning-fast response and customizable features. The Hero sensor provides flawless tracking, while the lightweight design ensures comfort during long gaming sessions. With programmable buttons and customizable RGB lighting, it\'s tailored to your gaming preferences. Dominate the competition with the Logitech G Pro Mouse.', 
+    0.17, 
+    'LGPM', 
+    'https://drive.google.com/file/d/1e7eMwnu59yojB9n4uO2cWI8gk3nTqImv/view?usp=sharing');
+-- product_id = 18
+call `group32_v1.0`.add_product(
+	'Corsair K70 Keyboard', 
+    'Electronics, Computer Accessories', 
+    'Corsair K70 Keyboard: The Corsair K70 Keyboard is a mechanical gaming keyboard that stands at the forefront of gaming peripherals. With its tactile and responsive Cherry MX switches, it provides a satisfying typing and gaming experience. The customizable RGB backlighting lets you create stunning lighting effects, and the durable aluminum frame ensures long-lasting performance. Whether you\'re a competitive gamer or a casual typist, the Corsair K70 delivers the precision and style you need.', 
+    0.9, 
+    'CK70', 
+    'https://drive.google.com/file/d/1dxfraytmfbvzF3V4nqyfUlGUvkJWCzfp/view?usp=sharing');
+-- product_id = 19
+call `group32_v1.0`.add_product(
+	'Barbie Dreamhouse', 
+    'Toys, Dolls & Accessories', 
+    'Barbie Dreamhouse: The Barbie Dreamhouse is a dream come true for young and young-at-heart Barbie enthusiasts. This fully furnished, interactive playset features multiple rooms, including a kitchen, living room, bedroom, and more. It\'s equipped with interactive accessories, such as a working elevator and a swimming pool with a slide. The Dreamhouse allows for endless imaginative play and storytelling, letting kids create their own Barbie adventures in style and comfort.', 
+    2.51, 
+    'BDH', 
+    'https://drive.google.com/file/d/1V694CCZJ17djg1TyBnGuXu24Ed8mhccz/view?usp=sharing');
+-- product_id = 20
+call `group32_v1.0`.add_product(
+	'Transformers Optimus Prime', 
+    'Toys, Action Figures', 
+    'Transformers Optimus Prime: The Transformers Optimus Prime figure brings the iconic Autobot leader to life. It seamlessly transforms from a powerful robot into a formidable truck, offering hours of exciting play for Transformers fans of all ages. With intricate details and articulation, Optimus Prime is ready to roll out and defend the universe against the Decepticons. This collectible figure is a must-have for fans of the legendary Transformers franchise.', 
+    0.55, 
+    'TOP', 
+    'https://drive.google.com/file/d/1-vs1eWSl92fEdZnza4DbOh-_hZwh7-FM/view?usp=sharing');
+-- product_id = 21
+call `group32_v1.0`.add_product(
+	'Dell Inspiron 14', 
+    'Electronics, Laptops', 
+    'Dell Inspiron 14: The Dell Inspiron 14 is a versatile laptop designed to meet your everyday computing needs. With its sleek design and reliable performance, it\'s ideal for work, school, or entertainment. The crisp display offers clear visuals, and the responsive keyboard ensures comfortable typing. Powered by efficient processors, it handles tasks with ease. The Inspiron 14 is a dependable and affordable choice for those seeking a balance of performance and portability.', 
+    1.55, 
+    'DI14', 
+    'https://drive.google.com/file/d/1_VSf8P1t6BrQZ60fGzBXb_I24i-OSDBp/view?usp=sharing');
+-- product_id = 22
+call `group32_v1.0`.add_product(
+	'Apple AirPods Pro', 
+    'Electronics, Audio & Headphones', 
+    'Apple AirPods Pro: The Apple AirPods Pro redefine the wireless earbud experience. With active noise cancellation, they immerse you in pure, high-fidelity sound. The customizable fit ensures comfort and stability, making them perfect for workouts, commutes, or long flights. The H1 chip enables seamless pairing with your Apple devices, and the intuitive controls allow you to adjust settings with a touch. Whether you\'re listening to music, taking calls, or enjoying podcasts, the AirPods Pro deliver an exceptional audio experience.', 
+    0.08, 
+    'AAP3', 
+    'https://drive.google.com/file/d/17PlnAfqa1vRMuMIbUZzhHGLiDEYgprTZ/view?usp=sharing');
+    
 -- ---------------------------------------------------------------------
 
 -- add items
