7406c2e99c6617c4736c23612909fdaf51015a45
Iruda Chirath <iruda.21@cse.mrt.ac.lk>
2023-10-26 20:42:28 +0530

    added .env file

--- server/index.js
+++ server/index.js
@@ -1,59 +1,62 @@
-const express = require('express');
+const express = require("express");
+
 const app = express();
 
-require('express-async-errors');
-require('body-parser');
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
 
-const db = require('./db');
-const cors = require('cors');
+require("express-async-errors");
+require("body-parser");
+
+const db = require("./db");
+const cors = require("cors");
 
 // Controllers
-const productRoutes = require('./controllers/products');
-const customerRoutes = require('./controllers/customers');
-const tempOrderRoutes = require('./controllers/temp-orders');
-const mainCategoryRoutes = require('./controllers/categories');
+const productRoutes = require("./controllers/products");
+const customerRoutes = require("./controllers/customers");
+const tempOrderRoutes = require("./controllers/temp-orders");
+const mainCategoryRoutes = require("./controllers/categories");
 
 // Functionalities
-const registerRoutes = require('./functionalities/register');
-const cartItemRoutes = require('./functionalities/cart');
-const loginRoutes = require('./functionalities/login');
+const registerRoutes = require("./functionalities/register");
+const cartItemRoutes = require("./functionalities/cart");
+const loginRoutes = require("./functionalities/login");
 
 // Admin
-const salesRoutes = require('./admin/sales');
-const orderRoutes = require('./admin/orders');
+const salesRoutes = require("./admin/sales");
+const orderRoutes = require("./admin/orders");
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 app.use(cors());
 
 // Controllers
-app.use('/shop', productRoutes);
-app.use('/customers', customerRoutes);
-app.use('/temp-orders', tempOrderRoutes);
-app.use('/main-categories', mainCategoryRoutes);
+app.use("/shop", productRoutes);
+app.use("/customers", customerRoutes);
+app.use("/temp-orders", tempOrderRoutes);
+app.use("/main-categories", mainCategoryRoutes);
 
 // Functionalities
-app.use('/register', registerRoutes);
-app.use('/cart',cartItemRoutes);
-app.use('/login',loginRoutes);
+app.use("/register", registerRoutes);
+app.use("/cart", cartItemRoutes);
+app.use("/login", loginRoutes);
 
 // Admin
-app.use('/sales',salesRoutes);
-app.use('/orders',orderRoutes);
+app.use("/sales", salesRoutes);
+app.use("/orders", orderRoutes);
 
-app.use((err,req,res,next)=>{
-    console.log(err);
-    res.status(err.status || 500).send("Something went wrong.");
-})
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.status || 500).send("Something went wrong.");
+});
 
 db.query("SELECT * FROM admin")
-    .then(data => {
-        console.log("DB connection succeeded.")
-        console.log(data)
-        app.listen(8000,()=>{
-            console.log("Server running on 8000.")
-        })
-    })
-    .catch(err => console.log("DB connection failed. " + err));
-
+  .then((data) => {
+    console.log("DB connection succeeded.");
+    console.log(data);
+    app.listen(8000, () => {
+      console.log("Server running on 8000.");
+    });
+  })
+  .catch((err) => console.log("DB connection failed. " + err));
