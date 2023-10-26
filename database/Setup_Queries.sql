285c87cb5185c68d64ce05f1fa78252dd5fb4726
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2023-10-26 07:33:28 +0530

    add insert statements
    Add insert statements for customer, card_detail and cart tables

--- database/Setup_Queries.sql
+++ database/Setup_Queries.sql
@@ -110,9 +110,10 @@ INSERT INTO `admin` (`Admin_username`, `Hashed_password`) VALUES
 );
 
 -- customer
+-- customer 1
 INSERT INTO `customer` (`Hashed_password`, `First_name`, `Last_name`, `Email`, `Phone_number`, `Address_line1`, `Address_line2`, `City`, `Province`, `Zipcode`, `Is_registered`) VALUES 
 (
-	'$2a$12$TIR.yPVwlysXVwdKlE/VBuXdDxkDPReKc3UfulSsrtFmvWM7v8mEG', 
+	'$2a$12$SZu9QokwMpuin9YwlT46Z.vSfjYGFiYvtjf2dvPpYOm5sW09osxk6', -- password: Lakshitha123
 	'Lakshitha', 
 	'Perera', 
 	'lakshitha.perera@gmail.com', 
@@ -124,9 +125,10 @@ INSERT INTO `customer` (`Hashed_password`, `First_name`, `Last_name`, `Email`, `
 	'60000', 
 	'1'
 );
+-- customer 2
 INSERT INTO `customer` (`Hashed_password`, `First_name`, `Last_name`, `Email`, `Phone_number`, `Address_line1`, `Address_line2`, `City`, `Province`, `Zipcode`, `Is_registered`) VALUES 
 (
-	'$2a$12$mNT3rQnvgwseYrh.Gw2uYuAOQwnWe0oNB.ueJBCGLT6Fe9gnZz/wC', 
+	'$2a$12$5kiPfTBOnvS83wFcjALGaOlTYA/Rc11TT/iEd2Ju2z3mzWVYm4WNm', -- password: Abcd1234
 	'Ravindu', 
 	'Silva', 
 	'ravindu.silva@gmail.com', 
@@ -138,8 +140,10 @@ INSERT INTO `customer` (`Hashed_password`, `First_name`, `Last_name`, `Email`, `
 	'80000', 
 	'1'
 );
-INSERT INTO `customer` (`First_name`, `Last_name`, `Email`, `Phone_number`, `Address_line1`, `City`, `Province`, `Zipcode`, `Is_registered`) VALUES 
+-- customer 3
+INSERT INTO `customer` (`Hashed_password`, `First_name`, `Last_name`, `Email`, `Phone_number`, `Address_line1`, `City`, `Province`, `Zipcode`, `Is_registered`) VALUES 
 (
+	'$2a$12$80efCEWWFKs6j3GrQiFUJOo.vRukoPq6RRxsmV76RRNm59OIcL4r.', -- password: NissanCar90
 	'Dilani', 
 	'Fernando', 
 	'dilani.fernando@gmail.com', 
@@ -150,9 +154,10 @@ INSERT INTO `customer` (`First_name`, `Last_name`, `Email`, `Phone_number`, `Add
 	'50000', 
 	'0'
 );
+-- customer 4
 INSERT INTO `customer` (`Hashed_password`, `First_name`, `Last_name`, `Email`, `Phone_number`, `Address_line1`, `Address_line2`, `City`, `Province`, `Zipcode`, `Is_registered`) VALUES 
 (
-	'$2a$12$YAdFxX09xqzcJtCrgQ2JEePZ5iEOSeTENlMFJoCIqy.Xrfg5T9jau', 
+	'$2a$12$ZYQ3ummi2UM5EB0h48oweuJCAOM9pPPpgvALzOBNprj13E9klpnUK', -- password: AntMan
 	'Rusira', 
 	'De Silva', 
 	'rusiradesilva33@gmail.com', 
@@ -164,9 +169,10 @@ INSERT INTO `customer` (`Hashed_password`, `First_name`, `Last_name`, `Email`, `
 	'80000', 
 	'1'
 );
+-- customer 5
 INSERT INTO `customer` (`Hashed_password`, `First_name`, `Last_name`, `Email`, `Phone_number`, `Address_line1`, `Address_line2`, `City`, `Province`, `Zipcode`, `Is_registered`) VALUES 
 (
-	'$2a$12$3hPxYD66X.Q4XQFoFT4DI.rukmDiPlj0JrZ2IjFihSTnG//2CioA2', 
+	'$2a$12$gfC2zAnF/sW7TROaCAdWQ.zYN/yBECwvg2KJT6oUJIaqNu.IlEM9q', -- password: sjkvsfi8HncUBEN()@_#
 	'Visitha', 
 	'Galegoda', 
 	'visitha.gale@gmail.com', 
@@ -178,17 +184,183 @@ INSERT INTO `customer` (`Hashed_password`, `First_name`, `Last_name`, `Email`, `
 	'62000', 
 	'1'
 );
+-- customer 6
+INSERT INTO `customer` (`Hashed_password`, `First_name`, `Last_name`, `Email`, `Phone_number`, `Address_line1`, `Address_line2`, `City`, `Province`, `Zipcode`, `Is_registered`) VALUES 
+(
+	'$2a$12$JymSFggFyAGKTL9AQrGkWeXTkUNAwWFZfbQxLUPAw1LZwVc2ISYTW', -- password: hhiidwdwdada
+	'Amara', 
+	'Wijesinghe', 
+	'amara.wijesinghe@gmail.com', 
+	'0712567832', 
+	'No.8, Fifth Lane', 
+	'Peradeniya Road', 
+	'Kandy', 
+	'Central', 
+	'20000', 
+	'1'
+);
+-- customer 7
+INSERT INTO `customer` (`Hashed_password`, `First_name`, `Last_name`, `Email`, `Phone_number`, `Address_line1`, `Address_line2`, `City`, `Province`, `Zipcode`, `Is_registered`) VALUES 
+(
+	'$2a$12$HAkxqrU1TQm7G9HkfUJNueV9fkh0zK3xltoTw4k5vqK/5kIOjQPma', -- password: GrandFinale
+	'Pasan', 
+	'Bandara', 
+	'pasan.bandara@gmail.com', 
+	'0717890456', 
+	'No.6, Sixth Lane', 
+	'Main Street', 
+	'Badulla', 
+	'Uva', 
+	'70000', 
+	'0'
+);
+-- customer 8
+INSERT INTO `customer` (`Hashed_password`, `First_name`, `Last_name`, `Email`, `Phone_number`, `Address_line1`, `Address_line2`, `City`, `Province`, `Zipcode`, `Is_registered`) VALUES 
+(
+	'$2a$12$Ic2IsQD./rJw4VxszP12GuB0zHHFtyejQ0Vgk71R1sVl60ZgjT7Gm', -- password: tired#boy
+	'Ruwan', 
+	'Liyanage', 
+	'ruwan.liyanage@gmail.com', 
+	'0715478923', 
+	'No.11, Elm Street', 
+	'Nuwara Eliya Road', 
+	'Nuwara Eliya', 
+	'Central', 
+	'22200', 
+	'1'
+);
+-- customer 9
+INSERT INTO `customer` (`Hashed_password`, `First_name`, `Last_name`, `Email`, `Phone_number`, `Address_line1`, `Address_line2`, `City`, `Province`, `Zipcode`, `Is_registered`) VALUES 
+(
+	'$2a$12$A8pKjkv2xbBL2YsKjBrKke8d8TnsP/.b2peJRUKJ3rOnAcdH1ERya', 
+	'Thilini', 
+	'Kumarasinghe', 
+	'thilini.kumara@gmail.com', 
+	'0716532489', 
+	'No.23, Park Road', 
+	'Colombo Road', 
+	'Rathnapura', 
+	'Sabaragamuwa', 
+	'70500', 
+	'0'
+);
+-- customer 10
+INSERT INTO `customer` (`Hashed_password`, `First_name`, `Last_name`, `Email`, `Phone_number`, `Address_line1`, `City`, `Province`, `Zipcode`, `Is_registered`) VALUES 
+(
+	'$2a$12$HKoOaOYkEPYbr.qxYCi4m.9mg9q6C6MG1oSEkl.x1VlIilsqEOz7u', -- LionKing
+	'Kushani', 
+	'Peris', 
+	'kushani.peris@gmail.com', 
+	'0718925634', 
+	'No.13, Green Avenue', 
+	'Jaffna', 
+	'Northern', 
+	'40000', 
+	'1'
+);
+-- customer 11
+INSERT INTO `customer` (`Hashed_password`, `First_name`, `Last_name`, `Email`, `Phone_number`, `Address_line1`, `Address_line2`, `City`, `Province`, `Zipcode`, `Is_registered`) VALUES 
+(
+	'$2a$12$HFxlfXseXef/FMylc.5nUurNWvywjk8.xFVqQDbB4tTst5zgqy.B.', 
+	'Nimal', 
+	'Rathnayake', 
+	'nimal.rathna@gmail.com', 
+	'0716352487', 
+	'No.25, First Cross Street', 
+	'Dambulla Road', 
+	'Dambulla', 
+	'Central', 
+	'21100', 
+	'1'
+);
+-- customer 12
+INSERT INTO `customer` (`Hashed_password`, `First_name`, `Last_name`, `Email`, `Phone_number`, `Address_line1`, `Address_line2`, `City`, `Province`, `Zipcode`, `Is_registered`) VALUES 
+(
+	'$2a$12$5nHX8xgPPgF6KpgVLzWqZeZHq6bA2v5yP.Vv.KndwLb6kNeh2Dypa', 
+	'Roshan', 
+	'Dias', 
+	'roshan.dias@gmail.com', 
+	'0719586742', 
+	'No.9, Lake Road', 
+	'Main Street', 
+	'Anuradhapura', 
+	'North Central', 
+	'50010', 
+	'0'
+);
+-- customer 13
+INSERT INTO `customer` (`Hashed_password`, `First_name`, `Last_name`, `Email`, `Phone_number`, `Address_line1`, `Address_line2`, `City`, `Province`, `Zipcode`, `Is_registered`) VALUES 
+(
+	'$2a$12$8TKLz2OZ5TP0p18Y9P8o8u/qSHLfyjddKE/vVHjYMSzlFGp.VVvqK', 
+	'Janani', 
+	'Gunasekara', 
+	'janani.guna@gmail.com', 
+	'0712358674', 
+	'No.15, Maple Street', 
+	'Ella Road', 
+	'Ella', 
+	'Uva', 
+	'90100', 
+	'1'
+);
+-- customer 14
+INSERT INTO `customer` (`Hashed_password`, `First_name`, `Last_name`, `Email`, `Phone_number`, `Address_line1`, `Address_line2`, `City`, `Province`, `Zipcode`, `Is_registered`) VALUES 
+(
+	'$2a$12$6xz8jPsQefiLWu5NWtUfOuzg66vX3iFQd2Kv3kT/F2e4oY9H0/TI6', 
+	'Samadhi', 
+	'Wickramasinghe', 
+	'samadhi.wick@gmail.com', 
+	'0716589321', 
+	'No.20, High Street', 
+	'Bandarawela Road', 
+	'Bandarawela', 
+	'Uva', 
+	'90150', 
+	'0'
+);
+-- customer 15
+INSERT INTO `customer` (`Hashed_password`, `First_name`, `Last_name`, `Email`, `Phone_number`, `Address_line1`, `Address_line2`, `City`, `Province`, `Zipcode`, `Is_registered`) VALUES 
+(
+	'$2a$12$yGwpTVu/YuOLZOL5SflTfeFs5.ZRUb/9DYJz8dUKu7QZBcSjKyOLC', 
+	'Dulaj', 
+	'Fernando', 
+	'dulaj.fernando@gmail.com', 
+	'0719563428', 
+	'No.4, River Drive', 
+	'Panadura Road', 
+	'Panadura', 
+	'Western', 
+	'12500', 
+	'1'
+);
+
+
 -- card details
 INSERT INTO `card_detail` (`Customer_id`, `Name_on_card`, `Card_number`, `Expiry_date`) VALUES ('1', 'Lakshitha P', '5391123412341234', '06/25');
 INSERT INTO `card_detail` (`Customer_id`, `Name_on_card`, `Card_number`, `Expiry_date`) VALUES ('2', 'Silva R', '5391234523452345', '06/27');
 INSERT INTO `card_detail` (`Customer_id`, `Name_on_card`, `Card_number`, `Expiry_date`) VALUES ('4', 'De Silva F B R L ', '5391567856785678', '01/27');
+INSERT INTO `card_detail` (`Customer_id`, `Name_on_card`, `Card_number`, `Expiry_date`) VALUES ('5', 'Visitha Galegoda', '5391777888999001', '11/31');
+INSERT INTO `card_detail` (`Customer_id`, `Name_on_card`, `Card_number`, `Expiry_date`) VALUES ('6', 'Amara Wijesinghe', '5391666777888999', '03/30');
+INSERT INTO `card_detail` (`Customer_id`, `Name_on_card`, `Card_number`, `Expiry_date`) VALUES ('8', 'Ruwan L K', '5391555666777888', '02/27');
+INSERT INTO `card_detail` (`Customer_id`, `Name_on_card`, `Card_number`, `Expiry_date`) VALUES ('10', 'Kushani P', '5391444555666777', '10/29');
+INSERT INTO `card_detail` (`Customer_id`, `Name_on_card`, `Card_number`, `Expiry_date`) VALUES ('11', 'Nimal Rathnayake', '5391333444555666', '05/28');
+INSERT INTO `card_detail` (`Customer_id`, `Name_on_card`, `Card_number`, `Expiry_date`) VALUES ('13', 'Janani G R', '5391222333444555', '09/27');
+INSERT INTO `card_detail` (`Customer_id`, `Name_on_card`, `Card_number`, `Expiry_date`) VALUES ('15', 'Dulaj Fernando', '5391111222333444', '06/26');
+
 
 -- cart
 INSERT INTO `cart` (`Customer_id`) VALUES ('1');
 INSERT INTO `cart` (`Customer_id`) VALUES ('2');
-INSERT INTO `cart` (`Customer_id`) VALUES ('3');
+-- INSERT INTO `cart` (`Customer_id`) VALUES ('3');
 INSERT INTO `cart` (`Customer_id`) VALUES ('4');
 INSERT INTO `cart` (`Customer_id`) VALUES ('5');
+INSERT INTO `cart` (`Customer_id`) VALUES ('6');
+INSERT INTO `cart` (`Customer_id`) VALUES ('8');
+INSERT INTO `cart` (`Customer_id`) VALUES ('10');
+INSERT INTO `cart` (`Customer_id`) VALUES ('11');
+INSERT INTO `cart` (`Customer_id`) VALUES ('13');
+INSERT INTO `cart` (`Customer_id`) VALUES ('15');
+
 
 -- cart item
 INSERT INTO `cart_item` (`Cart_id`, `Item_id`, `Quantity`) VALUES ('1', '1', '2');
