c83c4d144cf777405376e04b531d24e3147143f4
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2023-10-26 13:38:15 +0530

    create triggers after_customer_update/insert
    Create two triggers to add a cart when a customer is registered

--- database/SQL_Triggers.sql
+++ database/SQL_Triggers.sql
@@ -1,4 +1,4 @@
--- update the quantity of the item after an order is placed
+-- update the quantity of the item after an order is placed and delete the item from the cart table
 DROP TRIGGER IF EXISTS order_item_after_insert;
 
 DELIMITER //
@@ -10,18 +10,21 @@ BEGIN
     DECLARE current_cart_id INT;
     
     SELECT Quantity INTO current_quantity FROM item WHERE Item_id = NEW.Item_id;
-
+    -- check if the quantity is enough
     IF current_quantity < NEW.Quantity THEN
+        -- if not enough, set the new quantity to 0 and raise a warning
         SELECT 0 INTO new_quantity;
         SIGNAL SQLSTATE '01000' SET MESSAGE_TEXT = 'Exceeds the Quantity'; -- warning
     ELSE
+        -- if enough, set the new quantity to the current quantity minus the quantity ordered
         SELECT current_quantity - NEW.Quantity INTO new_quantity;
     END IF;
-
+    -- update the quantity of the item
     UPDATE item i
 	SET Quantity = new_quantity
 	WHERE i.Item_id = NEW.Item_id;
     
+    -- delete the item from the cart table
     SELECT DISTINCT(Cart_id) INTO current_cart_id
     FROM order_item oi
     JOIN `shop order` so USING (order_id)
@@ -31,3 +34,31 @@ BEGIN
 END//
 
 DELIMITER ;   
+
+
+-- when an unregistered user register as a customer, create a cart for the customer
+DROP TRIGGER IF EXISTS after_customer_update;
+
+DELIMITER //
+CREATE TRIGGER after_customer_update AFTER UPDATE 
+ON customer FOR EACH ROW 
+BEGIN
+    IF NEW.Is_registered = 1 THEN
+        INSERT INTO `cart` (`Customer_id`) VALUES (NEW.Customer_id);
+    END IF;
+END //
+DELIMITER ;
+
+
+-- create a cart for a customer when the customer is registered
+DROP TRIGGER IF EXISTS after_customer_insert;
+
+DELIMITER //
+CREATE TRIGGER after_customer_insert AFTER INSERT 
+ON customer FOR EACH ROW 
+BEGIN
+    IF NEW.Is_registered = 1 THEN
+        INSERT INTO `cart` (`Customer_id`) VALUES (NEW.Customer_id);
+    END IF;
+END //
+DELIMITER ;
