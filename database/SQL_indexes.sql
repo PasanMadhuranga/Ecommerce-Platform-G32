5a37b3f52e35ea1ff5b02d0e027c1b4645bf6eca
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2023-10-28 21:25:31 +0530

    create a fulltext index

--- database/SQL_indexes.sql
+++ database/SQL_indexes.sql
@@ -1,1 +1,5 @@
+-- create an index on the date column of the shop_order table to speed up the admin related procedures
 CREATE INDEX idx_date ON shop_order(Date);
+
+-- create a full text index on the title and description columns of the product table to speed up the search
+CREATE FULLTEXT INDEX idx_Title_Description ON product(Title, Description);
