5ac2e2c612d5ba45f496b7a88610634254ea14cb
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2023-10-29 05:41:23 +0530

    add Cart_id to "product_sales" view

--- database/SQL_views.sql
+++ database/SQL_views.sql
@@ -1,7 +1,7 @@
 DROP VIEW IF EXISTS product_sales;
 
 CREATE VIEW product_sales AS
-SELECT oi.Order_id, i.Product_id, oi.Item_id, oi.Unit_price, oi.Quantity, so.Date
+SELECT oi.Order_id, oi.Cart_id, i.Product_id, oi.Item_id, oi.Unit_price, oi.Quantity, so.Date
 FROM order_item oi
 JOIN item i USING (item_id)
 JOIN shop_order so USING (Order_id);
